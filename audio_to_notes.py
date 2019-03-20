45fb4100837a4cafaaf8f479738c7595d4c0329f
moocher <moocher@moocher.ru>
2019-03-20 10:32:24 +0300

    formatting changed

--- audio_to_notes.py
+++ audio_to_notes.py
@@ -17,7 +17,7 @@ def main():
     melody = source(args.input)
     melody_note = notes(samplerate=melody.samplerate)
 
-    print("%5s" % "time", "note")
+    print('time note')
 
     total_frames = 0
     while True:
@@ -25,7 +25,7 @@ def main():
         note = int(melody_note(samples)[0])
         if note:
             time = total_frames / float(melody.samplerate)
-            print("%.3f" % time, midi2note(note))
+            print(f'{time:.2f} {midi2note(note)}')
         total_frames += read
         if read < melody_note.hop_size:
             break
