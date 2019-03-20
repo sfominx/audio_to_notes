20721db8f5b6144413708aa8a083bc46616b6da8
moocher <moocher@moocher.ru>
2019-03-20 10:39:16 +0300

    Code divided into functions

--- audio_to_notes.py
+++ audio_to_notes.py
@@ -2,10 +2,29 @@
 Convert WAV file to notes
 """
 import argparse
+from typing import List, Tuple
 
 from aubio import midi2note, notes, source
 
 HOP_SIZE = 512
+
+
+def convert_wav_to_notes(input_file: str) -> List[Tuple[float, str]]:
+    """Convert simple single melody WAV file to notes"""
+    melody = source(input_file)
+    melody_note = notes(samplerate=melody.samplerate)
+    notes_ = []
+    total_frames = 0
+    while True:
+        samples, read = melody()
+        note = int(melody_note(samples)[0])
+        if note:
+            time = total_frames / float(melody.samplerate)
+            notes_.append((time, midi2note(note)))
+        total_frames += read
+        if read < melody_note.hop_size:
+            break
+    return notes_
 
 
 def main():
@@ -14,21 +33,11 @@ def main():
     parser.add_argument('input', type=str, help='input file')
     args = parser.parse_args()
 
-    melody = source(args.input)
-    melody_note = notes(samplerate=melody.samplerate)
+    notes_ = convert_wav_to_notes(args.input)
 
-    print('time note')
-
-    total_frames = 0
-    while True:
-        samples, read = melody()
-        note = int(melody_note(samples)[0])
-        if note:
-            time = total_frames / float(melody.samplerate)
-            print(f'{time:.2f} {midi2note(note)}')
-        total_frames += read
-        if read < melody_note.hop_size:
-            break
+    print('Time Note')
+    for time, note in notes_:
+        print(f'{time:.2f} {note}')
 
 
 if __name__ == '__main__':
